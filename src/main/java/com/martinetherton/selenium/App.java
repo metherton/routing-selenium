efa636a04b29d063f921c596fb19c1deed2cb898
metherton <martinetherton63@googlemail.com>
2013-10-14 20:32:01 +0200

    initial commit

---
+++ src/main/java/com/martinetherton/selenium/App.java
@@ -0,0 +1,13 @@
+package com.martinetherton.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
